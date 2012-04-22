d3e2ab62f65c003008dc733aef0a3832f9f9e7bb
Erik Dolor <erik.dolor@gmail.com>
2012-04-22 22:37:21 +0200

    Added GUIDHD.java

---
+++ src/minecraft/net/minecraft/src/sgc/GUIDHD.java
@@ -0,0 +1,10 @@
+package net.minecraft.src.sgc;
+
+public class GUIDHD {
+
+	/**
+	 * Won't be implemented for now, merely created to save this link:
+	 * http://www.youtube.com/watch?v=NnF6TyHjB1o
+	 */
+	
+}
